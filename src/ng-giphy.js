ca06523cad03dcad359c10d65ce11b9df7fa2c75
gillchristian <gillchristiang@gmail.com>
2016-02-14 20:12:08 -0300

    Added giphy-gif

--- src/ng-giphy.js
+++ src/ng-giphy.js
@@ -3,8 +3,12 @@
 
   angular.module('ng-giphy', [])
     .directive('giphyRand', giphyRand)
+    .directive('giphyGif', giphyGif)
     .factory('giphy', giphyService);
 
+  /**
+   * Directive: random gif
+   */
   function giphyRand() {
     return {
       scope: {
@@ -29,20 +33,69 @@
   }
 
 
+  /**
+   * Directive: Specific gif by id
+   */
+  function giphyGif() {
+    return {
+      scope: {
+        id    : '=',
+        rating: '='
+      },
+      controller: giphyGifController,
+      controllerAs: 'vm',
+      bindToController: true,
+      template: '<img ng-src="{{ vm.giphysrc }}">'
+    };
+  }
+
+  giphyGifController.$inject = ['giphy'];
+  function giphyGifController(giphy) {
+    var vm = this;
+
+    var id = vm.id || 'YyKPbc5OOTSQE';
+    giphy.random(id).then(function (res) {
+      vm.giphysrc = res;
+    });
+  }
+
+  /**
+   * Services to interact with the giphy API endpoints
+   */
   giphyService.$inject = ['$http'];
   function giphyService($http) {
-    var apiurl = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC';
+    var searchUrl = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC';
+    var byIdUrl = 'http://api.giphy.com/v1/gifs/%s?api_key=dc6zaTOxFJmzC'
 
     return {
-      random: random
+      random: random,
+      byId: byId
     };
 
+    /**
+     * Gets a random gif url
+     * 
+     * @param {string} query
+     * @return {string} gif url
+     */
     function random(q){
       var words = q.split(' ');
       q = words.join('+')
-      return $http.get(apiurl + '&q=' + q).then(function (res) {
-        console.log(res.data);
+      return $http.get(searchUrl + '&q=' + q).then(function (res) {
         return res.data.data[0].images.original.url;
+      });
+    }
+    
+   /**
+    * Returns a giphy url by query its ID.
+    *
+    * @param {string} gif id
+    * @return {string} gif url
+    */
+    function byId(id){
+      return $http.get( byIdUrl.replace('%s', id) ).then(function (res) {
+        console.log(res.data);
+        return res.data.url;
       });
     }
   }
