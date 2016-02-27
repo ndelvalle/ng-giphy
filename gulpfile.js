33f32fbb7ecbda8e4c21315ad3808572c03e73ad
ndelvalle <nicolas.delvalle@gmail.com>
2016-02-27 22:26:25 +0100

    Added gulp task to build javascript main file

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,42 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('build-js', function() {
-  return gulp.src('src/ng-giphy.js')
+var ngAnnotate = require('gulp-ng-annotate');
+var bytediff = require('gulp-bytediff');
+
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+
+var paths = {
+  source: 'src/ng-giphy.js',
+  dest  : 'dist'
+};
+
+gulp.task('jshint', function() {
+  return gulp.src(paths.source)
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('build-js', ['jshint'], function() {
+  return gulp.src(paths.source)
     .pipe(sourcemaps.init())
-    .pipe(concat('ng-giphy.min.js'))
-    .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist'));
+    .pipe(concat('ng-giphy.min.js', { newLine: ';' }))
+    // Annotate before uglify so the code get's min'd properly.
+    .pipe(ngAnnotate({
+        // true helps add where @ngInject is not used. It infers.
+        // Doesn't work with resolve, so we must be explicit there
+        add: true
+    }))
+    .pipe(bytediff.start())
+    .pipe(uglify({ mangle: true }))
+    .pipe(bytediff.stop())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.dest));
 });
 
-gulp.task('watch-js', function(){
-    gulp.watch('src/**/*.js', ['build-js']);  
+gulp.task('watch', function(){
+    gulp.watch('src/**/*.js', ['build-js']);
 });
 
-gulp.task('default', ['build-js', 'watch-js']);
+gulp.task('default', ['build-js']);
