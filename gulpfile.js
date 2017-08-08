f7b49c6768eac4d0898714f1f7e74c50e9f794bf
ndelvalle <nicolas.delvalle@gmail.com>
2017-08-08 14:35:49 -0300

    Add yarn file and update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
-var gulp        = require('gulp');
-var gutil       = require('gulp-util');
+var gulp       = require('gulp');
+var gutil      = require('gulp-util');
 
-var concat      = require('gulp-concat');
-var uglify      = require('gulp-uglify');
-var sourcemaps  = require('gulp-sourcemaps');
+var concat     = require('gulp-concat');
+var uglify     = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
-var ngAnnotate  = require('gulp-ng-annotate');
-var bytediff    = require('gulp-bytediff');
+var ngAnnotate = require('gulp-ng-annotate');
+var bytediff   = require('gulp-bytediff');
 
-var jshint      = require('gulp-jshint');
-var stylish     = require('jshint-stylish');
+var jshint     = require('gulp-jshint');
+var stylish    = require('jshint-stylish');
 
-var paths       = require('./helpers/paths');
-var tasks       = require('./helpers/tasks');
+var paths      = require('./helpers/paths');
+var tasks      = require('./helpers/tasks');
 
 gulp.task(tasks.js.hint, function() {
   return gulp.src(paths.source)
